7e4b84d9cb1c49defc6f774af76936731e1edd89
Andrew Rosen <andrew.rosen@temple.edu>
2021-07-07 19:45:20 -0400

    Adding more code while watching Adam Neely

--- code/linkedlist.py
+++ code/linkedlist.py
@@ -1,19 +1,34 @@
-class LinkedList(object):
-    
-    class Node(object):
-        def __init__(self, item) -> None:
+from typing import Generic, TypeVar
+
+
+E =  TypeVar('E')
+
+class LinkedList(Generic[E]):
+
+    class Node(Generic[E]):
+        def __init__(self, item: E) -> None:
             self.item = item
             self.next = None
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.head = None
         self.tail = None
         self.size = 0
     
-    def __len__(self):
+    def __len__(self) -> int: 
         return self.size
 
-    def add(self, index, item):
+    def getNode(self, index: int) -> Node:
+        current = self.head
+        for i in range(index):
+            current = current.next
+        return current
+
+    def add(self, item: E) -> bool:
+        self.add(index,index,item)
+        return True
+
+    def add(self, index: int, item: E) -> None:
         if(index < 0 or index > self.size):
             raise Exception("Invalid add at index " + str(index) +" with item" + str(item) +".")
         
@@ -34,11 +49,23 @@ class LinkedList(object):
 
         self.size += 1
 
-    def getNode(self,index):
-        current = self.head
-        for i in range(index):
-            current = current.next
-        return current
+    def remove(self, index: int) -> E:
+        if(index < 0 or index >= self.size):
+            raise Exception("Invalid remove at index " + str(index) +".")
+        
+        toReturn = None
+        if self.size == 1:
+            self.head = None
+            self.tail = None
+        elif index == 0:
+            toReturn = self.head.item
+            self.head = self.head.next
+
+        self.size -= 1
+        return toReturn
+
+
+
 
 l = LinkedList()
 print(len(l))
