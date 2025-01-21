f7b5aa062e352bc44f8e0f1b6c9f371c9d4155fe
Andrew Rosen <andrew.rosen@temple.edu>
2025-01-21 13:44:25 -0500

    More Stack work

--- code/stack.py
+++ code/stack.py
@@ -1,29 +1,34 @@
-# Original: Problem Solving with Algorithms and Data Structures using Python
-# Brad Miller and David Ranum, Luther College
-# https://runestone.academy/ns/books/published/pythonds3/BasicDS/ImplementingaStackinPython.html
 class Stack:
-    """Stack implementation as a list"""
+    class Node:
+        def __init__(self, item):
+            self.item = item
+            self.next = None
 
     def __init__(self):
-        """Create new stack"""
-        self._items = []
+        self.top = None
 
     def is_empty(self):
-        """Check if the stack is empty"""
-        return not bool(self._items)
-
-    def push(self, item):
-        """Add an item to the stack"""
-        self._items.append(item)
-
-    def pop(self):
-        """Remove an item from the stack"""
-        return self._items.pop()
+        return self.top is None
 
     def peek(self):
-        """Get the value of the top item in the stack"""
-        return self._items[-1]
+        return self.top.item
 
-    def size(self):
-        """Get the number of items in the stack"""
-        return len(self._items)
+    def pop(self):
+        toReturn = self.top.item
+        self.top = self.top.next
+        return toReturn
+
+    def push(self, item):
+        newTop = self.Node(item)
+        newTop.next = self.top
+        self.top = newTop
+        return item
+
+
+if __name__ == "__main__":
+    s = Stack()
+    for i in range(1, 11):
+        s.push(i)
+
+    while not s.is_empty():
+        print(s.pop())
